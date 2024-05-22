f2c2b4be12238a9e7c7fe447d263fd9144c886d6
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2024-05-22 01:31:29 -0300

    build to default target with on-demand crates and own functions

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@
 
 use core::panic::PanicInfo;
 
+// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     //C naming convention
     // this function is the entry point, since the linker looks for a function named `_start` by default
-    loop {}
-}
-
-// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
